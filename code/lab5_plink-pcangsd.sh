5149b325173b9a21b409bc047bfc104eed716043
Cappy <cappy.pugliese@gmail.com>
2025-10-02 17:25:15 -0400

    trying to get pcangsd to work

--- code/lab5_plink-pcangsd.sh
+++ code/lab5_plink-pcangsd.sh
@@ -1,6 +1,45 @@
+#!/bin/bash
+#SBATCH --job-name=pcangsd
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 8
+#SBATCH --mem=20G
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=caprina.pugliese@uconn.edu
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+########### script start
+
+hostname
+date
+
+cd /home/FCAM/cpugliese
+## creating environment
+module load anaconda3/4.1.1
+conda create -n pcangsd
+source activate pcangsd
+conda install pip
+pip install pcangsd
+
+## load modules
+module load pcangsd/1.0
+module load python/3.10.1
+
 ## set variables
-INDIR=/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/2025-26_Year-2/popgen/labs/cappys_labs_popgen25/data/03_lab5/01_plink
-OUTDIR=/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/2025-26_Year-2/popgen/labs/cappys_labs_popgen25/data/03_lab5/02_pcangsd
-PCANGSD="python3 /Users/caprinapugliese/.my-bin/pcangsd/pcangsd.py"
+INDIR=/home/FCAM/cpugliese/popgen/data/02_lab5/01_plink
+OUTDIR=/home/FCAM/cpugliese/popgen/data/02_lab5/02_pcangsd
+PCANGSD="python3 /isg/shared/apps/pcangsd/1.0/pcangsd/pcangsd.py"
+
+cd $OUTDIR
+## PCAnsd
+$PCANGSD -plink $INDIR/plink_muc19_data \
+-threads 8 \
+-out $OUTDIR/pcangsd_muc19_data
 
 $PCANGSD -p $INDIR/plink_muc19_data -o $OUTDIR/pcangsd_muc19_data --admix
+
+
+########### script end
