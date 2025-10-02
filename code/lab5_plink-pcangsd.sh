e0cd99217e7326cf47c34698a148b206db969b29
Cappy <cappy.pugliese@gmail.com>
2025-10-02 12:32:37 -0400

    Update lab5_plink-pcangsd.sh

--- code/lab5_plink-pcangsd.sh
+++ code/lab5_plink-pcangsd.sh
@@ -1,5 +1,6 @@
-
-
 ## set variables
-INDIR=/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/2025-26_Year-2/popgen/labs/cappys_labs/data/01_muc19_data
-OUTDIR=
+INDIR=/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/2025-26_Year-2/popgen/labs/cappys_labs_popgen25/data/03_lab5/01_plink
+OUTDIR=/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/2025-26_Year-2/popgen/labs/cappys_labs_popgen25/data/03_lab5/02_pcangsd
+PCANGSD="python3 /Users/caprinapugliese/.my-bin/pcangsd/pcangsd.py"
+
+$PCANGSD -p $INDIR/plink_muc19_data -o $OUTDIR/pcangsd_muc19_data --admix
