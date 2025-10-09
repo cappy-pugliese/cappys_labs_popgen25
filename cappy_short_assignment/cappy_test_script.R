2ab81ef0dce2b7c5bccf4cc18ad31ecce89086d2
Cappy <cappy.pugliese@gmail.com>
2025-10-09 18:54:16 -0400

    made some changes, mainly worked on the test script

--- cappy_short_assignment/cappy_test_script.R
+++ cappy_short_assignment/cappy_test_script.R
@@ -2,6 +2,21 @@
 source("cappy_short_assign_src.R")
 
 GT <- read.table("25_10-09_short_assign_fake_data_w_NAs.txt")
-pops <- read.table("25_10-09_short_assign_pop-file.txt")
+samples <- read.table("25_10-09_short_assign_pop-file.txt")
 
-freq_GT(GT)
+freq_GT(GT$V2)
+#[1] 0.3
+freq_GT(GT$V17)
+#[1] 0.45
+
+process_GT(GT, samples)
+## does not seem to work for my test data?
+
+sample_het(GT$V2)
+#[1] 0.4307692
+sample_het(GT$V17)
+#[1] 0.5076923
+
+fst_site()
+
+cor_mat(GT)
