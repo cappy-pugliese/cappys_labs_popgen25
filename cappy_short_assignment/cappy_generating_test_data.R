632d3468298405d67bc2ce770e9079aed8715d19
Cappy <cappy.pugliese@gmail.com>
2025-10-11 12:58:38 -0400

    made a lot of changes whoops

--- cappy_short_assignment/cappy_generating_test_data.R
+++ cappy_short_assignment/cappy_generating_test_data.R
@@ -1,5 +1,8 @@
 #setwd("/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/2025-26_Year-2/popgen/labs/cappys_labs_popgen25/cappy_short_assignment")
 
+# load libraries
+library(dplyr)
+
 ######## generating the data ########
 
 ### population file ###
@@ -7,13 +10,13 @@
 indivs <- sprintf("IND_%d", 1:20)
 
 #Assigning the populations: first 5 are pop1, next 5 are pop2, etc.
-pop <- rep(c("POP1","POP2","POP3","POP4"), each=5)
+population <- rep(c("POP1","POP2","POP3","POP4"), each=5)
 
 #Put all variables data together
-pop_test_file <- data.frame(indivs,pop)
+pop_test_file <- data.frame(indivs,population)
 
 #creating the file
-write.table(pop_test_file,file="25_10-09_short_assign_pop-file.txt",row.names=FALSE,quote=FALSE)
+write.csv(pop_test_file,file="25_10-09_short_assign_pop-file.csv",row.names=FALSE,quote=FALSE)
 
 
 ### test data file ###
@@ -25,10 +28,13 @@ allele_data <- t(replicate(length(indivs), rbinom(n_sites, 1, 0.5)))
 # 0.5 in rbinom is the probability of recieving a 1 vs a 0
 
 # putting it all together
-fake_data <- data.frame(indivs, allele_data)
+fake_data <- data.frame(row.names = indivs, allele_data)
+
+rename_with(fake_data, .cols = everything(), ...)
+
 
 #creating the file
-write.table(fake_data,file="25_10-09_short_assign_fake-data.txt",row.names=FALSE,quote=FALSE)
+write.csv(fake_data,file="25_10-09_short_assign_fake-data.csv",quote=FALSE)
 
 
 ### adding missing data ###
@@ -43,6 +49,6 @@ for (ind in seq_len(nrow(allele_data))) {
 }
 
 # putting it into one dataframe
-missing_data <- data.frame(indivs, allele_data)
+missing_data <- data.frame(row.names = indivs, allele_data)
 
-write.table(missing_data,file="25_10-09_short_assign_fake_data_w_NAs.txt",row.names=FALSE,col.names=FALSE,quote=FALSE)
+write.csv(missing_data,file="25_10-09_short_assign_fake_data_w_NAs.csv",quote=FALSE)
--- cappy_short_assignment/cappy_test_script.R
+++ cappy_short_assignment/cappy_test_script.R
@@ -1,22 +1,29 @@
 #setwd("/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/2025-26_Year-2/popgen/labs/cappys_labs_popgen25/cappy_short_assignment")
 source("cappy_short_assign_src.R")
 
-GT <- read.table("25_10-09_short_assign_fake_data_w_NAs.txt")
-samples <- read.table("25_10-09_short_assign_pop-file.txt")
+# load in libraries
+library(dplyr)
+library(readr)
+
+GT <- read.csv("25_10-09_short_assign_fake_data_w_NAs.csv")
+samples <- read.csv("25_10-09_short_assign_pop-file.csv")
+
+# fix samples table
+
 
 #GT_no_NAs <- read.table("25_10-09_short_assign_fake-data.txt")
 # ^^for debugging in case there's an NA issue
 
 ## working so far, just need to make it work with all the data
-freq_GT(GT$V2)
-#[1] 0.3
-freq_GT(GT$V17)
+freq_GT(GT$X2)
 #[1] 0.45
+freq_GT(GT$X17)
+#[1] 0.5
 
-sample_het(GT$V2)
-#[1] 0.4307692
-sample_het(GT$V17)
+sample_het(GT$X2)
 #[1] 0.5076923
+sample_het(GT$X17)
+#[1] 0.5128205
 
 ## still need to get to work
 fst_site()
