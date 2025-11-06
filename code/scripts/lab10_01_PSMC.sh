7078610e350e994e8fb95554605c482a4d466f44
Cappy <cappy.pugliese@gmail.com>
2025-11-06 13:02:29 -0500

    finished lab 10

--- code/scripts/lab10_01_PSMC.sh
+++ code/scripts/lab10_01_PSMC.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -c 2
-#SBATCH --mem=2G
+#SBATCH --mem=3G
 #SBATCH --partition=general
 #SBATCH --qos=general
 #SBATCH -o %x_%j.out
@@ -15,7 +15,6 @@ hostname
 date
 
 ## load modules
-module load psmc/0.6.5
 module load bcftools/1.20
 module load seqtk/1.3
 module load samtools/1.20
@@ -32,16 +31,20 @@ cd $OUTDIR
 
 ## run programs
 ### will make psmc requisite file
-#bcftools view -s $sample ${INDIR}/${VCF} -Oz -o ${sample}.vcf.gz
+bcftools view -s $sample ${INDIR}/${VCF} -Oz -o ${sample}.vcf.gz
 
-#seqtk mutfa ${INDIR}/${REF} <(gzip -dc ${sample}.vcf.gz|${psmcdir}/utils/vcf2snp.pl -) | seqtk seq | ${psmcdir}/utils/fq2psmcfa - > ${sample}.psmcfa
+seqtk mutfa ${INDIR}/${REF} <(gzip -dc ${sample}.vcf.gz|${psmcdir}/utils/vcf2snp.pl -) | seqtk seq | ${psmcdir}/utils/fq2psmcfa - > ${sample}.psmcfa
 
-# filter psmc req file
-samtools mpileup -C50 -uf ${REF}.fa ${sample}.bam | bcftools view -c - \
-      | vcfutils.pl vcf2fq -d <lower_quartile_depth> -D <upper_quartile_depth> | gzip > ${sample}.fq.gz
-
-${psmcdir}/utils/fq2psmcfa -q20 ${sample}.fq.gz > ${sample}.psmcfa
+# filter psmc req file (own data, don't need to do)
+#samtools mpileup -C50 -uf ${REF}.fa ${sample}.bam | bcftools view -c - | vcfutils.pl vcf2fq -d <lower_quartile_depth> -D <upper_quartile_depth> | gzip > ${sample}.fq.gz
+#${psmcdir}/utils/fq2psmcfa -q20 ${sample}.fq.gz > ${sample}.psmcfa
 
 # finally run psmc
-${psmc}/psmc -N25 -t15 -r5 -p "4+25*2+4+6" -o ${sample}.psmc ${sample}.psmcfa
+${psmcdir}/psmc -N25 -t15 -r5 -p "4+25*2+4+6" -o ${sample}.psmc ${sample}.psmcfa
+
+# make graph
+## will need to change around parameters
+## current parameters are ment to work with human data
+${psmcdir}/utils/psmc_plot.pl ${sample} ${sample}.psmc
+
 ########### script end
