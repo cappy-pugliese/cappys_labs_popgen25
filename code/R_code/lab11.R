907ad161409f21ff2ca671cb06310aaa4ed56313
Cappy <cappy.pugliese@gmail.com>
2025-11-13 18:32:04 -0500

    added lab 11

--- code/R_code/lab11.R
+++ code/R_code/lab11.R
@@ -1,6 +1,12 @@
+# failed to get tess3r to install
+# issue with c++? not able to find 'iterator'
+# posted a question on stack overflow, will have to see if anyone is able to help me figure out what's going on
+
 library(tess3r)
 library(readr)
 library(maps)
+library(raster)
+library(terra)
 
 setwd("/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/2025-26_Year-2/popgen/labs/cappys_labs_popgen25/plots")
 
@@ -9,3 +15,15 @@ samples_table = read_csv("/Users/caprinapugliese/Documents/School/Uconn/2024-26_
 plot(samples_table$Long,samples_table$Lat,pch=16)
 map(add=T,interior=F)
 geno = as.matrix(read.table("/Users/caprinapugliese/Documents/School/Uconn/2024-26_Grad_School/2025-26_Year-2/popgen/labs/PopGen25/labs/data/nacanids.lfmm"))
+
+
+coords = as.matrix(samples_table[,2:3])
+tess3_res = tess3(X=geno,coord=coords,K=1:8,method="projected.ls",ploidy=2,openMP.core.num=4)
+
+
+your_K = ???
+ancestry_matrix =  qmatrix(tess3_res,K=your_K)
+ancestry_barplot = barplot(ancestry_matrix,border=NA,space=0,xlab="Ind",ylab="Ancestry")
+
+
+area_plot = plot(ancestry_matrix,coords,method="map.max",interpol=FieldsKrigModel(10))
